1e4c29a4bc0915f76c6f9cdfaa47fc3d43ba6cb2
St-Pardon <okc4pardon@gmail.com>
2022-12-07 11:33:56 +0000

    :new: task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# handle nginx multiple concurrent request
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
